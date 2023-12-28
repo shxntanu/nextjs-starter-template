e9be4274f7082b8ed69686b5e312c45a0e4e1883
Shantanu <shantanuwable2003@gmail.com>
2023-12-28 17:49:16 +0530

    feat: add SessionProvider to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { ConfigProvider } from "antd";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/system";
 import theme from "@/config/theme/themeConfig";
 
-const App = ({ Component, pageProps }: AppProps) => (
-    <ConfigProvider theme={theme}>
-        <NextUIProvider>
-            <Component {...pageProps} />
-        </NextUIProvider>
-    </ConfigProvider>
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => (
+    <SessionProvider session={session}>
+        <ConfigProvider theme={theme}>
+            <NextUIProvider>
+                <Component {...pageProps} />
+            </NextUIProvider>
+        </ConfigProvider>
+    </SessionProvider>
 );
 
 export default App;
